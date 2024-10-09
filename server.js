b5d669bf3b2f5ef33e306c42f58448e5e6331652
maruti-panchal <panchal.maruti70@gmail.com>
2024-10-09 22:33:19 +0530

    added helmet and limiter to prevent DDOS attacks and headers security

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const rateLimiter = require("express-rate-limit");
 const connectDB = require("./config/db");
+const helmet = require("helmet");
 
 dotenv.config();
 connectDB();
 
+const limiter = rateLimiter({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP,Please try again letter",
+});
+
 const app = express();
+
+app.use(helmet());
+app.use("/api", limiter);
 app.use(express.json());
 
 // User and Admin Routes
